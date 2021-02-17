c67e1ca3574ad6ce2a791e529a3036f9bd1f9fce
njnxes <najo6550@colorado.edu>
2021-02-17 15:37:14 -0700

    + Board_Tests.java

---
+++ src/edu/colorado/group18/Board_Tests.java
@@ -0,0 +1,27 @@
+package edu.colorado.group18;
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.assertEquals;
+
+class boardTests {
+
+    private Board B;
+
+    @Before
+    public void setUp() throws Exception {
+        int cols = 5; // X
+        int rows = 5; // Y
+        B = new Board(rows, cols);
+    }
+
+    @Test
+    public void checkRows() {
+        assertEquals(5, B.getY());
+    }
+
+    @Test
+    public void checkCols() {
+        assertEquals(5, B.getX());
+    }
+
+}

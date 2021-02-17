b378509421be599996e67c96f06bf9a950f67e7a
EvanKuehr <evan.kuehr@colorado.edu>
2021-02-17 13:54:18 -0700

    Created an Abstract Cell Class
    Will have two children classes

---
+++ src/edu/colorado/group18/Cell.java
@@ -0,0 +1,18 @@
+package edu.colorado.group18;
+
+public abstract class Cell {
+    private boolean hit; //determines whether or not this cell has been hit before
+
+    public Cell() {
+        hit = false;
+    }
+
+    public boolean getHitStatus() {
+        return hit;
+    }
+
+    public void updateHitStatus(boolean newStatus) {
+        hit = newStatus;
+        return;
+    }
+}

3e1ed04168d6497e9f144783ae6856217bec5f99
njnxes <najo6550@colorado.edu>
2021-02-20 15:37:57 -0700

    + Board.placeShip() + Board.strike()

--- src/edu/colorado/group18/Board.java
+++ src/edu/colorado/group18/Board.java
@@ -4,7 +4,7 @@ public class Board {
 
     private int cols; // X-dimension
     private int rows; // Y-dimension
-    public Cell cells[][] = new Cell[rows][cols]; // 2-D array of Cell objects
+    public ShipCell cells[][] = new ShipCell[rows][cols]; // 2-D array of Cell objects
 
     public Board(int y, int x) {
         cols = x;
@@ -13,6 +13,22 @@ public class Board {
 
     public int getX() { return cols; }
     public int getY() { return rows; }
-    public Cell getCell(int col, int row) { return cells[row][col]; } // Return cell at coordinates (x, y)
+    public Cell getCell(int col, int row) { return cells[row][col]; } // Return cell at coordinates (col, row)
+
+    public void placeShip(Ship ship, int col, int row, String orientation) { // Place ship at given coordinates
+        if (col <= cols && row <= rows) { // Initial bounds check
+            if (orientation == 'h') { // Fill cells to the right of (col, row)
+                if (ship.getLength() <= cols - col) { cells[row][col] = ship; }
+            }
+            else { // Fill cells above (col, row)
+                if (ship.getLength() <= rows - row) { cells[row][col] = ship; }
+            }
+        }
+    }
+
+    public void strike(int col, int row) {
+        if (cells[row][col].getShipRef() != null) { cells[row][col].setHitStatus(true); } // Cell is occupied
+        else { return; }
+    }
 
 }

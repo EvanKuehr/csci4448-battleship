c369d5ee3ceebe9ea87ea49d5abf8914edde8741
EvanKuehr <evan.kuehr@colorado.edu>
2021-02-20 23:27:37 -0700

    Made a TestSuite that can be used to run every test

--- src/edu/colorado/group18/Card.java
+++ src/edu/colorado/group18/Card.java
@@ -1,5 +1,6 @@
 package edu.colorado.group18;
 
+//This class will be fully implemented in a future milestone
 public class Card {
     public double cost;
     public String ability;
--- src/edu/colorado/group18/Main.java
+++ src/edu/colorado/group18/Main.java
@@ -1,10 +1,22 @@
 package edu.colorado.group18;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
   
 public class Main {
-
     public static void main(String[] args) {
-        // write your code here
-         Ship ship = new Ship("submarine", 3);
-         System.out.println(ship.getLength());
+        //Tutorial followed to run a JUnit test suite: https://bit.ly/3pJ8vzb
+        Result result = JUnitCore.runClasses(TestSuite.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+            failure.getException().printStackTrace();
+        }
+
+        if (result.wasSuccessful()) {
+            System.out.println("All tests were successful!");
+        }
+        else {
+            System.out.println("One or more tests have failed");
+        }
     }
 }

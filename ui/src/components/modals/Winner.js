ab94eb24997aa7d6688ae9552193702b0d21509b
njnxes <najo6550@colorado.edu>
2021-04-23 17:43:15 -0600

    + Winner.js (modal) + Winner.scss (modal)

---
+++ ui/src/components/modals/Winner.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./style/Winner.scss";
+
+const WinnerModal = () => (
+    <div className={styles.winnerModal}>
+        <p className={styles.youWin}>YOU WIN</p>
+        <img
+            alt=""
+            className={styles.victoryDefeat1}
+            src="https://static.overlay-tech.com/assets/ecc35279-75f9-4f7a-bd66-94fb51d3d92f.png"
+        />
+        <div className={styles.group4}>
+            <p className={styles.continue}>CONTINUE</p>
+        </div>
+    </div>
+);
+
+export default WinnerModal;
